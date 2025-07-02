d733304f5aad2a15ca35006c49d6909e92d63b84
Batkhuu Byambajav <batkhuu10@gmail.com>
2025-07-02 17:54:24 +0900

    :hammer::green_heart: Refactor workflows to streamline version bumping and changelog updates

---
+++ scripts/release.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+set -euo pipefail
+
+
+## --- Base --- ##
+# Getting path of this script file:
+_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+_PROJECT_DIR="$(cd "${_SCRIPT_DIR}/.." >/dev/null 2>&1 && pwd)"
+cd "${_PROJECT_DIR}" || exit 2
+
+
+# Loading .env file:
+if [ -f ".env" ]; then
+	# shellcheck disable=SC1091
+	source .env
+fi
+
+if [ -z "$(which git)" ]; then
+	echo "[ERROR]: 'git' not found or not installed!"
+	exit 1
+fi
+
+if [ -z "$(which gh)" ]; then
+	echo "[ERROR]: 'gh' not found or not installed!"
+	exit 1
+fi
+
+if ! gh auth status >/dev/null 2>&1; then
+	echo "[ERROR]: You need to login: 'gh auth login'"
+	exit 1
+fi
+## --- Base --- ##
+
+
+## --- Variables --- ##
+# Flags:
+_IS_BUILD=false
+## --- Variables --- ##
+
+
+## --- Main --- ##
+main()
+{
+	## --- Menu arguments --- ##
+	if [ -n "${1:-}" ]; then
+		for _input in "${@:-}"; do
+			case ${_input} in
+				-b | --build)
+					_IS_BUILD=true
+					shift;;
+				*)
+					echo "[ERROR]: Failed to parsing input -> ${_input}"
+					echo "[INFO]: USAGE: ${0}  -b, --build"
+					exit 1;;
+			esac
+		done
+	fi
+	## --- Menu arguments --- ##
+
+
+	if [ "${_IS_BUILD}" == true ]; then
+		./scripts/build.sh -c
+	fi
+
+	_cur_version="$(./scripts/get-version.sh)"
+	echo "[INFO]: Creating release for version: 'v${_cur_version}'..."
+	gh release create "v${_cur_version}" ./dist/* --generate-notes
+	echo "[OK]: Done."
+}
+
+main "${@:-}"
+## --- Main --- ##
