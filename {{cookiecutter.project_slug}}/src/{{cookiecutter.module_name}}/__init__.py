bd506624cbbf1735042fce75550fb4a253eb59f4
Batkhuu Byambajav <batkhuu10@gmail.com>
2025-09-07 15:25:06 +0900

    :bug: Remove dotenv loading and reorder exports for improved clarity in __init__.py

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/__init__.py
@@ -1,16 +1,10 @@
-# flake8: noqa
-
-from dotenv import load_dotenv
-
-load_dotenv(override=True)
-
 from .__version__ import __version__
 from .config import ModelConfigPM
 from ._base import SimpleModel
 
 
 __all__ = [
-    "SimpleModel",
-    "ModelConfigPM",
     "__version__",
+    "ModelConfigPM",
+    "SimpleModel",
 ]
